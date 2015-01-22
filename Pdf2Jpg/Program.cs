af7915a7671c71fb7a24100559d913c8a924c871
Paul Huizer <paulhuizer@gmail.com>
2015-01-22 10:00:07 +0100

    Initial commit

---
+++ Pdf2Jpg/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Pdf2Jpg
+{
+    class Program
+    {
+        private static string _ghostScriptLibPath = "unknown";
+        private static string _pdfFullFilename = "unknown";
+        private static string _outputImageFileName = "unknown";
+
+        static void Main(string[] args)
+        {
+            PrintIntro();
+
+
+            Assembly ass = Assembly.GetExecutingAssembly();
+            _ghostScriptLibPath = System.IO.Path.GetDirectoryName(ass.Location);
+            _ghostScriptLibPath = _ghostScriptLibPath + @"\gsdll32.dll";
+
+            if (args.Length != 1)
+            {
+                PrintHelp();
+                Environment.Exit(0);
+            }
+
+            if (args.Length == 1)
+            {
+                _pdfFullFilename = args[0];
+                _outputImageFileName = _pdfFullFilename.Substring(0, _pdfFullFilename.LastIndexOf('.')) + ".jpg";
+            }
+
+            Console.WriteLine("GhostScriptlibPath=" + _ghostScriptLibPath);
+            Console.WriteLine("PdfFullFn=" + _pdfFullFilename);
+            Console.WriteLine("OutputImageFileName=" + _outputImageFileName);
+            
+            ConvertPdf2Jpg rasterizer = new ConvertPdf2Jpg();
+            rasterizer.Start(_ghostScriptLibPath, _pdfFullFilename, _outputImageFileName);
+        }
+
+        static private void PrintIntro()
+        {
+            Console.WriteLine("Pdf2Jpg.exe  A handy tool to convert a pdf file to jpg");
+
+        }
+        static private void PrintHelp()
+        {
+            Console.WriteLine("Pdf2Jpg requires 1 parameter, ie the full pathname to the pdf to convert.");
+            Console.WriteLine("The output jpg file will be in the same folder, with the same base filename as the source pdf file.");
+            Console.WriteLine("The output jpg file will have the fileextension .jpg");
+            Console.WriteLine();
+            Console.WriteLine("Example:");
+            Console.WriteLine("pdf2jpg c:\test\brochure.pdf");
+            Console.WriteLine("Output : c:\test\brochure.jpg");
+            Console.WriteLine("Multi-page pdfs will be stored in the same pdf.");
+            Console.WriteLine();
+            Console.WriteLine("See the documentation on github for installation and configuration info.");
+            Console.WriteLine("Recommended usage is to add pdf2jpg to the fileexplorer contextmenu");
+        }
+    }
+}
---
+++ Pdf2Jpg/Rasterize2Img.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Collections.Generic;
+
+// required Ghostscript.NET namespaces
+using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+
+namespace Pdf2Jpg
+{
+    public class ConvertPdf2Jpg
+    {
+        private GhostscriptVersionInfo _ghostScriptVersion = null;
+
+        public void Start(String ghostScriptLibPath, String inputPdfPath, string outputFileName)
+        {
+            int target_x_dpi = 300;
+            int target_y_dpi = 300;
+
+            _ghostScriptVersion = new GhostscriptVersionInfo(new Version(0, 0, 0), ghostScriptLibPath, string.Empty, GhostscriptLicense.GPL);
+            GhostscriptRasterizer rasterizer = new GhostscriptRasterizer();
+
+            var width = 0;
+            var height = 0;
+
+            rasterizer.Open(inputPdfPath, _ghostScriptVersion, false);
+            List<Bitmap> bmpList = new List<Bitmap>();
+
+            // Get an image for each page in the pdf
+            for (int pageNumber = 1; pageNumber <= rasterizer.PageCount; pageNumber++)
+            {
+                Image img = rasterizer.GetPage(target_x_dpi, target_y_dpi, pageNumber);
+                if (img.Width > width)
+                    width = img.Width;
+                height += img.Height;
+
+
+                Bitmap bmp = new Bitmap(img);
+                Bitmap bmp2 = (Bitmap)bmp.Clone();
+                bmpList.Add(bmp2);
+            }
+
+
+            // Add each image to the output bitmap, using canvas as the drawing vehicle 
+            var bitmap = new Bitmap(width, height);
+            var canvas = Graphics.FromImage(bitmap);
+            var curHeight = 0;
+            for (int i = 0; i < bmpList.Count; i++)
+            {
+                canvas.DrawImage(bmpList[i], new Point(0, curHeight));
+                curHeight += bmpList[i].Height;
+            }
+            canvas.Save();
+
+            // Save the output bitmap to file.
+            bitmap.Save(outputFileName, ImageFormat.Jpeg);
+        }
+    }
+}
+
